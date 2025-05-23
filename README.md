f94c782927a711bca37d01ded265ec0eb8f16afc
AminoGycoSide148 <funnyyellowdoge4@gmail.com>
2025-05-23 15:22:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ A 1.12.2 backport for the very simple mod that adds various decorational markings.
+ the 1.20.1 version -> https://github.com/Aminoglycoside148/modernmarkings_1.20.x
+ 
+ Based on the template mod by sa-shiro -> https://github.com/sa-shiro/Minecraft-Forge-1.12.2-BaseMod
---
+++ src/main/java/com/aminoglycoside/modernmarkings/base/init/ModItems.java
@@ -0,0 +1,34 @@
+package com.aminoglycoside.modernmarkings.base.init;
+
+import com.aminoglycoside.modernmarkings.base.items.ItemBase;
+import com.aminoglycoside.modernmarkings.base.util.Resource;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.*;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraftforge.common.util.EnumHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModItems {
+
+    /**
+ * Storing all Mod Items in an Array List
+ */
+public static final List<Item> ITEMS = new ArrayList<Item>();
+    /**
+     * function  to create new Items
+     */
+
+
+    public static final Item CHALK = new ItemBase("chalk");
+    /** function to create new Food */
+    // https://minecraft.gamepedia.com/Hunger
+    // public static final Item FOOD_NAME = new FoodBase("food_name", 2, 3.5f, false);
+
+    /** function to create new Food with Effects */
+    // public static final Item FOOD_NAME = new FoodEffectBase("food_name", 2, 3.5f, false,
+    //		new PotionEffect(MobEffects.HASTE, 60*20, 0, false, true));
+}
